61d71bb20a31705ac26a3216a893ddfe5fff3858
commandus <andrei.i.ivanov@commandus.com>
2022-12-27 12:16:14 +0900

    1

--- src/main/cpp/subst.h
+++
@@ -1,4 +0,0 @@
-#define open(file, flags, ...) open_c(file, flags)
-#define close(fd) close_c(fd)
-#define printf(args...) printf_c(args)
-#define fprintf(fd, args...) printf_c(args)
--- src/main/java/com/commandus/lgw/PayloadListener.java
+++
@@ -1,8 +0,0 @@
-package com.commandus.lgw;
-
-public interface PayloadListener {
-    void onValue(final Payload value);
-    void onInfo(final String msg);
-    void onConnected(final boolean on);
-    void onDisconnected();
-}
